c64854fe3fcaedcf11c8a529672b0c3bc67c592e
WilliamTRev <william.terry@revature.com>
2025-11-11 09:54:47 -0500

    function scope notes

--- Week1/Examples/JS/HelloJSFunctionScopes/JSFunctionsScopes.js
+++ Week1/Examples/JS/HelloJSFunctionScopes/JSFunctionsScopes.js
@@ -20,3 +20,24 @@ anon(); //using the anonymous function
 
 let arrow = () => {console.log("I'm an arrow function!")}
 arrow();
+
+//callback functions
+function func1(x){
+    console.log("Inside func1");
+    console.log("x = " + x)
+}
+
+function func2(y, cb){
+    console.log("Inside func2");
+    cb(y); //so cb is intended to be a function, and take y as an argument
+}
+
+func2(9,func1); //so let's call func2, and pass in func1 as the "cb" argument
+
+//let's pass in an arrow function as the "cb" argument instead
+
+func2(5, (x)=>{console.log("You gave the arrow function: "+x)});
+
+// in these two cases, func1 and the arrow function are the callback functions func2 is invoking!
+
+//Finally lets look at closures
