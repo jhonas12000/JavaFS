f3cd09aeb5a0da96299205ac9ddfc78bf739235a
WilliamTRev <william.terry@revature.com>
2025-11-13 10:17:39 -0500

    exploring-ts

---
+++ Week1/TypeScript/exploring-ts/js/app.js
@@ -0,0 +1,33 @@
+"use strict";
+console.log("Hello typescript");
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(value) {
+        if (value < 0) {
+            throw new Error("Age cannot be negative");
+        }
+        this._age = value;
+    }
+    toString() {
+        return `Person(name: ${this._name}, age: ${this._age})`;
+    }
+}
+//Usage 
+const person1 = new Person("Alice", 30);
+console.log(person1.toString());
+person1.name = "Will";
+person1.age = 31;
+console.log(person1.toString());
+//# sourceMappingURL=app.js.map
