e6ce7611798ee63bdbfd328d89671612eebac0c1
WilliamTRev <william.terry@revature.com>
2025-11-14 10:29:57 -0500

    adding first component

---
+++ Week1/Examples/React/HelloReact/my-app/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+/* The App.tsx is the root of out app (at least when it comes to rendering stuff on the page)
+Most of the code we write will go elsewhere in the src folder, but the App.tsx cntrols WHAT gets rendered
+We put the components we want to render in the div of the return()
+We can render standalone elements within the div as well, but we usually won't do it
+
+*/
+
+function App() {
+
+  return (
+
+    <div className = "App">
+      <header className = "App-header">
+        <h1> Hello React! We'll make great modern front ends with you :)</h1>
+        <p> We are writing in TSX, which is like HTML for React TS</p>
+        <h3>React will reload the webpage with every saved change to the App</h3>
+      </header>
+    </div>
+
+    
+  );
+}
+
+export default App
