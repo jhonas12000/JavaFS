d3b473e1cca0e17263622c8fa79114fed3794953
WilliamTRev <william.terry@revature.com>
2025-11-18 11:32:23 -0500

    user added

---
+++ Week2/Examples/P1DemoFrontEnd/p1logindemo/src/Components/User/UserTable.tsx
@@ -0,0 +1,80 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import type { User } from "../../Interfaces/User"
+import { Container, Table } from "react-bootstrap"
+
+
+export const UserTable: React.FC = () => {
+
+    //state object to store the user Array from the backend
+
+    const [users, setUsers] = useState<User[]>([])
+
+    //useEffect - we'll call a GET request for all users when the component loads
+
+    useEffect(() => {
+        //TODO: make sure the user is a manager, redirect them to login if not
+
+        getAllUsers()
+
+    }, [])//we want this to run once on load, so we use []
+
+
+    //Function to get all users from the backend (HTTP request)
+    const getAllUsers = async () => {
+
+        try {
+            const response = await axios.get("http://localhost:8080/users", { withCredentials: true })
+            //Again, we need withCredentials if the request requires specific session info 
+            //(existence of a session, role stored in the session, etc)
+
+            //TODO: error throwing code
+
+            console.log(response.data) //print out the data just to see it
+
+            //store the user data in our "users" state object
+            setUsers(response.data)
+
+        } catch {
+            alert("Something went wrong tryng to fetch users")
+        }
+    }
+    //function that does a fake update delete (wanna show how to extract data from a map)
+    const updateUser = (user:User) => {
+        alert("User"+user.userId +" has been fake updated or deleted")
+
+        //TODO: Could definitely make another call to getAllUsers for automatic updates
+        //TODO2: Cache the list of users and update THAT so we don't make a repeat DB call
+        
+    }
+
+    return(
+        <div>
+            <Container className="d-flex flex-column align-items-center mt-3">
+                <h3>Users:</h3>
+                <Table className="table-dark table-hover tabled-striped w-50">
+                    <thead>
+                        <tr>
+                            <th>User Id</th>
+                            <th>Username</th>
+                            <th>Role</th>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-seconday">
+                        {users.map((user:User)=>(
+                    <tr key={user.userId}>
+                    <td key={user.userId}/>
+                    <td key={user.username}/>
+                    <td key={user.role}/>
+                    </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                
+            </Container>
+
+        </div>
+    )
+
+}
