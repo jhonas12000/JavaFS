4853e5a10cd6c1434ff452e548c3743240cd70ce
WilliamTRev <william.terry@revature.com>
2025-11-20 11:45:51 -0500

    Junit Tests added

--- Week3/Examples/HelloHibernate/HelloHibernate/src/test/java/org/example/daos/BookDAOTest.java
+++ Week3/Examples/HelloHibernate/HelloHibernate/src/test/java/org/example/daos/BookDAOTest.java
@@ -2,23 +2,23 @@ package org.example.daos;
 
 import org.example.models.Author;
 import org.example.utils.HibernateUtil;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.example.models.Book;
 
+import java.util.List;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BookDAOTest {
     
     private static BookDao dao;
     
     @BeforeAll 
     static void setup(){
-        HibernateUtil.getSession();
         dao = new BookDao();
     }
     
     @Test
+    @Order(1)
     void testInsertBook(){
         Book b = new Book();
         Author a = new Author("Test Author","Author Test",1000);
@@ -32,6 +32,7 @@ public class BookDAOTest {
     }
 
     @Test
+    @Order(2)
     void testSelectBookById(){
         Book b = dao.selectBookById(1);
 
@@ -39,7 +40,24 @@ public class BookDAOTest {
         Assertions.assertEquals("JUnit Test Book", b.getTitle());
     }
 
+    @Test
+    @Order(3)
+    void testFindAllBooks(){
+        List<Book> books = dao.findAllBooks();
+        //System.out.println(books);
+        Assertions.assertFalse(books.isEmpty());
+    }
 
+    @Test
+    void testUpdateBook(){
+        Book b = dao.selectBookById(1);
+        b.setTitle("Updated Title");
 
+        dao.updateBook(b);
+
+        Book updated = dao.selectBookById(1);
+
+        Assertions.assertEquals("Updated Title",updated.getTitle());
+    }
 
 }
