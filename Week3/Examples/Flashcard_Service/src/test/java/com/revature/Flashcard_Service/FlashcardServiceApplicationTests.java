6f3b6a7dcdbe73f7ea7ac3df1d03f4d8b4245906
WilliamTRev <william.terry@revature.com>
2025-11-24 11:01:26 -0500

    spring data jpa example

---
+++ Week3/Examples/Flashcard_Service/src/test/java/com/revature/Flashcard_Service/FlashcardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Flashcard_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
